54fbcbb0a2d41631aab3d2797d24e52812c1a58d
1001ms <dimitriwzk@gmail.com>
2024-06-16 18:41:07 +0200

    Update ShowBoxAction.php

--- gift.appli/src/app/actions/ShowBoxAction.php
+++ gift.appli/src/app/actions/ShowBoxAction.php
@@ -5,11 +5,20 @@ namespace gift\appli\app\actions;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use gift\appli\core\services\coffret\CoffretService;
+use gift\appli\core\services\auth\AuthService;
+use gift\appli\app\utils\CsrfService;
 use Slim\Views\Twig;
 
 class ShowBoxAction extends AbstractAction {
     public function __invoke(Request $request, Response $response, array $args): Response {
         $view = Twig::fromRequest($request);
+        $user = null;
+        if (!AuthService::isAuthenticate()) {
+            return $view->render($response, 'error.html.twig', [
+                'message_error' => 'Vous devez être connecté pour effectuer un paiement.',
+                'code_error' => 403
+            ]);
+        }
         $token = $args['token'];
 
         $coffretService = new CoffretService();
@@ -24,6 +33,9 @@ class ShowBoxAction extends AbstractAction {
 
         return $view->render($response, 'show_box.html.twig', [
             'coffret' => $coffret,
+            'userIsLoggedIn' => AuthService::isAuthenticate(),
+            'csrf' => CsrfService::generate(),
+            'user' => $user,
         ]);
     }
 }
