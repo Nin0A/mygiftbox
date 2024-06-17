d520a2d80fbe27c406ae952e48f108a710885eb4
1001ms <dimitriwzk@gmail.com>
2024-06-17 11:31:36 +0200

    API pb reglé

--- gift.api/public/index.php
+++ gift.api/public/index.php
@@ -1,5 +1,5 @@
 <?php
 declare(strict_types=1);
-require_once __DIR__ . '/../src/vendor/autoload.php'; /* vendor -> autoload */
-$app = require_once __DIR__ . '/../src/infrastructure/conf/bootstrap.php'; /* application boostrap */
+require_once __DIR__ . '/src/vendor/autoload.php'; /* vendor -> autoload */
+$app = require_once __DIR__ . '/src/infrastructure/conf/bootstrap.php'; /* application boostrap */
 $app->run();
--- gift.api/src/infrastructure/conf/bootstrap.php
+++ gift.api/src/infrastructure/conf/bootstrap.php
@@ -8,21 +8,6 @@ $app->addRoutingMiddleware();
 $app->addErrorMiddleware(true, false, false);
 //$app->setBasePath('/projets/mygiftbox/gift.appli/public');
 $app = (require_once __DIR__ . '/routes.php')($app);
-
-$twig = \Slim\Views\Twig::create(
-    '../src/app/views/',
-    [/*'cache' => './src/app/views/cache',*/
-        'auto_reload' => true,
-        'strict_variables' => true
-    ]
-);
-$app->add(\Slim\Views\TwigMiddleware::create($app, $twig));
-
-$twig->getEnvironment()
-    ->addGlobal('globals', [
-        'img_dir' => '/img'
-    ]);
-
 session_start();
 
 return $app;
